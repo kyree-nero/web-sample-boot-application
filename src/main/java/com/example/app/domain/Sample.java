3cb926f81f57fb76b401f8132d01dfc5e475e03a
userx <userx@192.168.50.136>
2020-12-14 23:10:46 -0500

    add custom validator ex

--- src/main/java/com/example/app/domain/Sample.java
+++ src/main/java/com/example/app/domain/Sample.java
@@ -2,11 +2,22 @@ package com.example.app.domain;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.example.app.domain.validator.BadContentConstraint;
 
 public class Sample{
 	private Long id;
 	@NotBlank
-	@Pattern(regexp = "^[A-Za-z0-9\\s]+$", message= "characters must be alphanumeric or space")
+	@Pattern(
+			regexp = "^[A-Za-z0-9\\s]+$", 
+			message= "characters must be alphanumeric or space"
+	)
+	@Size(
+			min=1, max=100, 
+			message="characters must be less than 100 characters long"
+	)
+	@BadContentConstraint
 	private String content;
 	private Long version;
 	
